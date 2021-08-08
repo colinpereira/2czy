621eb051928146d6116e59171840fe5039ede6f6
colinpereira <64594861+colinpereira@users.noreply.github.com>
2021-08-08 17:09:58 +0400

    picker

--- src/Components/Model/Model.js
+++ src/Components/Model/Model.js
@@ -1,63 +1,79 @@
 import ReactDOM from "react-dom";
-import React, { useRef, useState, Suspense } from "react";
+import React, { useRef, useState, Suspense, useEffect } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { Environment, OrbitControls, useGLTF } from "@react-three/drei";
+import { proxy, useSnapshot } from "valtio";
+import { useProxy } from "valtio/macro";
+import { HexColorPicker } from "react-colorful";
+import "./Model.css";
 
-// function Model(props) {
-//   const group = useRef();
-//   const { nodes, materials } = useGLTF("logo.glb");
-//   return (
-//     <group ref={group} {...props} dispose={null}>
-//       <mesh
-//         geometry={nodes.website.geometry}
-//         material={materials["default"]}
-//         rotation={[Math.PI / 2, 0, 0]}
-//         scale={0.26}
-//       />
-//     </group>
-//   );
-// }
+const state = proxy({
+  current: null,
+  items: {
+    materials: "#fff",
+  },
+});
 
 function Model(props) {
   const group = useRef();
+  const snap = useSnapshot(state, { sync: true });
   const { nodes, materials } = useGLTF("model.glb");
-
-  //   return (
-  //     <group ref={group} {...props} dispose={null}>
-  //       <mesh geometry={nodes.shoe.geometry} material={materials.laces} />
-  //       <mesh geometry={nodes.shoe_1.geometry} material={materials.mesh} />
-  //       <mesh geometry={nodes.shoe_2.geometry} material={materials.caps} />
-  //       <mesh geometry={nodes.shoe_3.geometry} material={materials.inner} />
-  //       <mesh geometry={nodes.shoe_4.geometry} material={materials.sole} />
-  //       <mesh geometry={nodes.shoe_5.geometry} material={materials.stripes} />
-  //       <mesh geometry={nodes.shoe_6.geometry} material={materials.band} />
-  //       <mesh geometry={nodes.shoe_7.geometry} material={materials.patch} />
-  //     </group>
-  //   );
+  const [hover, setHover] = useState(null);
 
   return (
-    <group ref={group} {...props} dispose={null}>
+    <group
+      ref={group}
+      {...props}
+      dispose={null}
+      onPointerOver={(e) => (
+        e.stopPropagation(), setHover(e.object.material.name)
+      )}
+      onPointerOut={(e) => e.intersections.length === 0 && setHover(null)}
+      onPointerMissed={() => (state.current = null)}
+      onPointerDown={(e) => (
+        e.stopPropagation(), (state.current = e.object.material.name)
+      )}
+    >
       <mesh
         geometry={nodes["2_world_obj"].geometry}
+        material-color={snap.items.materials["Material.001"]}
         material={materials["Material.001"]}
         rotation={[Math.PI / 2, 0, 0]}
-        scale={1.4}
+        scale={1.5}
       />
     </group>
   );
 }
 
+const Picker = () => {
+  const snap = useSnapshot(state, { sync: true });
+  return (
+    <div style={{ display: snap.current ? "block" : "none" }}>
+      <h1>{snap.current}</h1>
+      {console.log(state.items)}
+      <HexColorPicker
+        className="picker"
+        color={snap.items[snap.current]}
+        onChange={(color) => (state.items[snap.current] = color)}
+      />
+    </div>
+  );
+};
+
 const ModelComp = (props) => {
   return (
-    <Canvas>
-      <ambientLight intensity={0.5} />
-      <spotLight intensity={0.5} position={[5, 20, 20]} />
-      <Suspense fallback={null}>
-        <Model />
-        <Environment files="royal_esplanade_1k.hdr" />
-      </Suspense>
-      <OrbitControls />
-    </Canvas>
+    <>
+      <Picker />
+      <Canvas>
+        <ambientLight intensity={0.5} />
+        <spotLight intensity={0.5} position={[5, 20, 20]} />
+        <Suspense fallback={null}>
+          <Model />
+          <Environment files="royal_esplanade_1k.hdr" />
+        </Suspense>
+        <OrbitControls />
+      </Canvas>
+    </>
   );
 };
 
