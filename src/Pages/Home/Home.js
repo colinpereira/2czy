eb30badc29469afd1ecd4f1d6e9167f94eeba838
colinpereira <64594861+colinpereira@users.noreply.github.com>
2021-08-09 11:54:35 +0400

    video back

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Home.css";
 import useMousePosition from "../../Hooks/useMouse";
+import backVideo from "../../Assets/Videos/backVideo.mp4";
 
 // Component imports
 import Navbar from "../../Components/Navbar/Navbar";
@@ -14,6 +15,7 @@ function Home() {
   return (
     <div className="homeContainer">
       <Navbar />
+      <video src={backVideo} autoPlay loop muted />
       <div className="ThreeDModel">
         <ModelComp />
       </div>
